06c52901bd1ac2e68424663a316debf14e2f2844
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-21 11:44:18 +0200

    primitiveTypes

---
+++ primitiveTypes.js
@@ -0,0 +1,11 @@
+// we have two categories of data types
+// primitive types <string,number,boolean and undefined,null>
+// reference types <>
+
+// primitive types
+let name = "Lucky"; //string literal
+let age = "18"; //Number literal
+let isApproved = true; //boolean literal <true or false>
+let firstName = undefined; //undefined literal
+let lastName = null; //null literal
+// we use null in a situation where we wanna clear a value of a variable
