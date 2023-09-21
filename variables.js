119167c440a0e6e7e45b53b39546bdbe8f831901
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-21 11:28:50 +0200

    variables

---
+++ variables.js
@@ -0,0 +1,26 @@
+// using let to declare a variable as ES6
+let myName="Lucky";
+console.log(myName);
+
+// rules for declaring a variable
+// ===============================
+
+
+// variables can not be a reserved  keyword
+// they should be meaningful
+// they can not start with a  number
+// can not contain a space or a hyphen (-)
+// we use camelCase convention to name a variable in javascript
+
+// the meaning of a variable to be meaningful
+let firstName = 'Ineza';
+console.log(firstName);
+
+// ways of declaring multiple variables at the same time
+let secondName,lastName;
+
+// the modern best practice to declare multiple variables
+secondName = 'Lucky';
+lastName='Believe';
+
+// the above codes is about assigning values to the variables that have been declared
