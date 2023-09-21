bdfc0ad6999638290d2973ab9bfa2329d9ada8cb
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-21 15:17:30 +0200

    operators

---
+++ operators.js
@@ -0,0 +1,26 @@
+// in javascript we have <Arthimetic operators,assignment operators,comparision operators,bitwise operators,Logical operators>
+
+// let's start with arthmetric operators
+let x = 8;
+let y = 4;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// increment and decrement operators (++)(--)
+
+console.log(x++);
+console.log(x);
+
+// decrement
+
+console.log(--x);
+console.log(x);
+
+// assignment operators
+
+let z = 4;
