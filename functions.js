da0d1b1e65a0d9263cecc5d771d6d77768cd875e
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-21 14:35:15 +0200

    functions

---
+++ functions.js
@@ -0,0 +1,15 @@
+// a function performs a task
+
+function greet() {
+  console.log("hello world");
+}
+
+// function call
+greet();
+// function with a parameter
+
+function call(name) {
+    console.log("hello"+name);
+}
+call("john");
+
