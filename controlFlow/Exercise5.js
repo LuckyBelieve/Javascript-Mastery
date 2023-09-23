a6f8070ea5d238b5e16337844f040f7639e0c9c8
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-24 00:16:49 +0200

    implement truthy values

--- controlFlow/Exercise5.js
+++ controlFlow/Exercise5.js
@@ -1,16 +1,10 @@
 // a function to return the number of truthy values in the array given
+let numbers = ["", 2, 0, 3, 9, undefined, null, 4, 5, 6];
+console.log(countTruthy(numbers));
 function countTruthy(array) {
   let count = 0;
   for (let key in array) {
-    if (
-      array[key] === undefined ||
-      array[key] === 0 ||
-      array[key] === null ||
-      array[key] === false ||
-      array[key] === NaN ||
-      array[key] === ""
-    ) {
-      return;
-    } else count++;
+    if (array[key]) count += 1;
   }
+  return count;
 }
