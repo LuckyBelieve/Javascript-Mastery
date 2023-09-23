3ef32ca19d684142cfc67851e0f65c00be78b771
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-23 18:42:17 +0200

    us of for of in js

--- controlFlow/ForIn.js
+++ controlFlow/ForIn.js
@@ -1,14 +1,23 @@
 let person = {
-firstName: 'John',
-location:"Kigali",
-age:30
-}
+  firstName: "John",
+  location: "Kigali",
+  age: 30,
+};
 
-for(let key in person){
-    console.log(key,person[key]);
+for (let key in person) {
+  console.log(key, person[key]);
 }
 
 let colors = ["orange", "red", "green", "yellow"];
-for(let index in colors){
-    console.log(index,colors[index]);
-};
+for (let index in colors) {
+  console.log(index, colors[index]);
+}
+
+// we basically use for in loops to iterate through the objects properties and on the
+// other hand we use for of loop to iterate through indices of the array
+
+// for of loop
+
+for(let color of colors) {
+    console.log(color);
+}
