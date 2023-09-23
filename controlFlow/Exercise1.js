0ee1f5778979170b265149bf8017e68dde61848a
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-23 18:50:55 +0200

    complete exercise one

---
+++ controlFlow/Exercise1.js
@@ -0,0 +1,8 @@
+// a function that takes two numbers and returns the maximum number
+
+function maximumNumber(a, b) {
+  return a > b ? a : b;
+}
+ let maximum = maximumNumber(4,5);
+ console.log(maximum);
+
