b8bf8fb2a30dbf76872b4d1ea4cd7e91a6dbac10
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-23 18:58:57 +0200

    exercise 2

--- controlFlow/Exercise1.js
+++ controlFlow/Exercise1.js
@@ -1,8 +1,17 @@
 // a function that takes two numbers and returns the maximum number
 
+// Exercises 1
+
 function maximumNumber(a, b) {
   return a > b ? a : b;
 }
- let maximum = maximumNumber(4,5);
- console.log(maximum);
+let maximum = maximumNumber(4, 5);
+console.log(maximum);
 
+//  exercise 2
+
+function isLandscape(width, height) {
+  return width > height;
+}
+
+console.log(isLandscape(30, 10));
